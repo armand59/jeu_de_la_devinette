768a63e35dd9d327c019a3b43d8ed629e37f4acf
armand59 <armand.lienard@hotmail.fr>
2019-11-21 16:23:00 +0100

    version2.0

--- script.js
+++ script.js
@@ -1,82 +1,57 @@
-var nb= nb_aleatoire(1, 100);
-var nombreDessai = 10;
-var saisie;
-var message = 'entre 1 et 100';
 
-document.getElementById("bouton").addEventListener()
-
-function nb_aleatoire(min, max)
-{
-    var nb = min + (max-min+1)*Math.random();//
-    return Math.floor(nb);//pour avoir un nombre entier
-}
-
-do{
-    saisie = prompt(message);//pour afficher la boîte de dialogue
-    nombreDessai= 10 ;//pour définir le nombre d'essai
-
-    if(saisie>nb)// si la saisie est supérieur au nombre alors "c'est moins"
-        message ="C'est moins";
-    else//pour tout le reste c'est plus!!
-
-        message = " C'est plus ! !";
-
-}
-
-while (saisie != nb);//a partir du moment où  la saisie est différente du nombre aléatoire
-
-alert('bravo tu as gagné !');//quand la condition est exacte
-
-
-function manche(min, max)
-{
-    var nb = nb_aleatoire(min, max);
-    var nombreDessai =10;
+function f() {
+    var message = "Choisi un nombre entre 1 et 100";
     var saisie;
-    var message = 'entre 1 et 100';
+    var nb = Math.floor(Math.random() * 100) + 1;
+    var nombreDessai = 0;
 
-    do{
-        saisie = prompt(message);
-        if(saisie ==null) return 0;
-
-        nombreDessai;
-        if (saisie>nb)
-            message = "c'est moins! ";
-        else
-            message = " c'est plus !";
-    }
-    while(saisie != nb);
-
-    return nombreDessai;
-}
-
-
-
-function partie(min, max)
-{
-
-    var nombreDessai= 10;
-    var score;
-    var continuer;
-    var bestscore = 0;
+    
 
     do {
-        score = manche(min, max)
-        if (score)
+        saisie = parseInt(prompt(message));//pour afficher la boîte de dialogue
+        nombreDessai++;
+        console.log(saisie);
+        if (saisie > nb) // si la saisie est supérieur au nombre alors "c'est moins"
         {
-            nombreDessai;
-            if (score < bestscore || bestscore == 0);
-            bestscore = score;
-            continuer=confirm( 'Tu as gagné en' + score + 'coups!! Jouer encore ?');
+            message = "C'est moins";
         }
-    else
-        continuer = false;
-    }
-    while(continuer);
-    alert ("Tu as gagné en" + score + "manche(s).\n)Ton meilleur score est de" + bestscore + "coups !");
-    return bestscore;
 
+        else if (saisie < nb)
+        {
+            message = " C'est plus !!";
+        }
+
+        else
+        {
+            continuer = confirm("bravo tu as gagné en " + nombreDessai + " coups! \nVeux tu rejouer ?");
+            break;
+        }
+
+        if (nombreDessai >= 10)
+        {
+            alert(" PERDU !! ");
+            break;
+        }
+
+    }
+
+    while (saisie !== nb);
 }
+
+document.getElementById("boutonstart").addEventListener("click", f);
+f();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
